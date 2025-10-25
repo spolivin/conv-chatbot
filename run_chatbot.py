43a8eb620960012dc34a489ec1f6e99d8f356f09
Sergey Polivin <s.polivin@gmail.com>
2025-10-25 16:15:53 +0200

    feat: add saving and loading of chat history and saved chats displaying

--- run_chatbot.py
+++ run_chatbot.py
@@ -1,17 +1,60 @@
+import argparse
+import sys
+
 import torch
 
-from model_wrapper import ChatModel
-from session import ChatSession
+from chatbot_api import ChatModel, ChatSession
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model-name",
+    type=str,
+    help="Model name for assistant",
+    default="Qwen/Qwen2.5-1.5B-Instruct",
+)
+parser.add_argument(
+    "--sys-prompt",
+    type=str,
+    help="System prompt",
+    default="You are a helpful and concise assistant.",
+)
+parser.add_argument("--chat-id", type=str, help="Conversation ID", default=None)
+args = parser.parse_args()
+
+def display_chat_history(history: list[dict[str, str]]):
+    """Displays all messages belonging to the chat.
+
+    Args:
+        history (list[dict[str, str]]): Chat history.
+    """
+    for turn in history:
+        message = turn["content"]
+        if turn["role"] == "user":
+            print(f"You: {message}")
+        elif turn["role"] == "assistant":
+            print(f"Assistant: {message}\n")
+        else:
+            pass
 
 def main():
-    chat = ChatSession(system_prompt="You are a helpful and concise assistant.")
-    model = ChatModel(model_name="Qwen/Qwen2.5-1.5B-Instruct")
+    chat = ChatSession(system_prompt=args.sys_prompt)
+    model = ChatModel(model_name=args.model_name)
 
-    print(f"Chatbot '{model.model_name}' ready! Type 'quit' to exit.")
+    # Loading a specific chat history (if provided in args)
+    if args.chat_id is not None:
+        try:
+            chat.load_history(chat_id=args.chat_id)
+            display_chat_history(history=chat.get_history())
+        except FileNotFoundError:
+            print(f"Chat #{args.chat_id} is not found. Aborting chatbot.")
+            sys.exit(1)
+
     while True:
         user_input = input("You: ")
         if user_input.lower() == "quit":
+            # Saving chat history
+            chat.save_history()
+            print("\nHave a nice day!")
             break
 
         chat.add_message(role="user", content=user_input)
